87675ff37fb2c50fd0298ac202ee03a78125c6a6
Nish Tewari <nishtew@gmail.com>
2024-03-10 15:27:04 -0400

    Update Lab 1

--- Lab 1/t04.py
+++ Lab 1/t04.py
@@ -5,9 +5,8 @@ A simple user friendly program to greet the user!
 Author: Nish Tewari 
 ID:     190684430
 Email:  tewa4430@mylaurier.ca 
-Last Edited = "2023-11-05"
+Last Edited = "03-10-2024"
 ------------------------------------------------------------------------
 """
 name = input("Please enter your name: ")
-print("Pleased to meet you,", name)
-
+print("Please to meet you,", name)
--- Lab 1/t06.py
+++ Lab 1/t06.py
@@ -1,15 +1,18 @@
 """
 ------------------------------------------------------------------------
-Calculating the user's pay with salary and bonus
+Calculating the user's pay with salary and bonus!
 ------------------------------------------------------------------------
 Author: Nish Tewari 
 ID:     190684430
 Email:  tewa4430@mylaurier.ca 
-Last Edited = "2023-11-05"
+Last Edited = "03-10-2024"
 ------------------------------------------------------------------------
 """
+# User enters their name 
+name = input("Please enter your name: ")
+
 # User Input 
-print("Hello User, today we will calculate your total pay at your workplace!")
+print("Hello", name + ",","today we will calculate your total pay at your workplace!")
 salary = int(input("What is your job salary? "))
 bonus  = int(input("What is the bonus you receive from work? "))
 
@@ -17,4 +20,4 @@ bonus  = int(input("What is the bonus you receive from work? "))
 total_pay = salary + bonus 
 
 # Print the total pay for the user
-print('Calculating your overall pay. Your pay is', total_pay, '$')
+print('Calculating your overall pay! Your pay is $', total_pay)
